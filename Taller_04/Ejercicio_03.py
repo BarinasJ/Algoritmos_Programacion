dc8730b33e315d936f43149f5a1d22567e7e790c
BarinasJ <98492642+BarinasJ@users.noreply.github.com>
2022-02-20 21:37:20 -0500

    Create Ejercicio_03.py

---
+++ Taller_04/Ejercicio_03.py
@@ -0,0 +1,10 @@
+"""
+Entradas ----
+Salidas --> la suma de todos los numeros pares 
+Suma --> int --> S
+"""
+S = 0
+# Caja negra 
+for i in range(98,1003,2):
+    S += i 
+print(f"\n{S}\n") # Salida
