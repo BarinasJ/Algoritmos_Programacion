125a39b6c6cee34b4579441c93d15d6abf5acae4
BarinasJ <98492642+BarinasJ@users.noreply.github.com>
2022-02-20 21:37:56 -0500

    Create Parcial_prueba.py

---
+++ Taller_04/Parcial_prueba.py
@@ -0,0 +1,16 @@
+# c=1
+# b=c
+# a=b
+# if(a==b and c<=a and a>=b or c!=b):
+#     print (True)
+# else:
+#     print(False)
+
+# x=False
+# if not x:
+#     print("yes")
+# else:
+#     print("No")
+
+palabra="VivaElParoNacional "
+print(palabra[15])
